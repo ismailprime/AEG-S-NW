379a280cddc2e2a3b71162ca1a2ffd60289e4180
ismailprime <isseat@hotmail.com>
2026-08-05 16:28:06 +0300

    Update buttons.js

--- interactions/buttons.js
+++ interactions/buttons.js
@@ -1,1 +1,50 @@
+module.exports=
+async(interaction)=>{
 
+
+if(
+interaction.customId==="stats"
+){
+
+interaction.reply({
+
+ephemeral:true,
+
+content:
+`
+📊 AEGİS NW İSTATİSTİK
+
+👥 Üye:
+${interaction.guild.memberCount}
+
+🤖 Bot:
+Aktif
+
+`
+
+});
+
+
+}
+
+
+
+if(
+interaction.customId==="ticket"
+){
+
+interaction.reply({
+
+ephemeral:true,
+
+content:
+"🎫 Ticket paneli hazırlanıyor."
+
+});
+
+
+}
+
+
+
+}
