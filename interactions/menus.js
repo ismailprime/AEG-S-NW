f62745732cfb6240bb5b44df6acee129049971c9
ismailprime <isseat@hotmail.com>
2026-08-05 16:29:39 +0300

    Update menus.js

--- interactions/menus.js
+++ interactions/menus.js
@@ -1,1 +1,24 @@
+module.exports=
+async interaction=>{
 
+
+if(
+interaction.customId==="ticket_category"
+){
+
+
+let category =
+interaction.values[0];
+
+
+
+require("../systems/ticket")
+(interaction,category);
+
+
+
+}
+
+
+
+}
